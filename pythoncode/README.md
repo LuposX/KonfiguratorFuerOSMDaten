bb565a3f4e4fb5a058d6cb833cc05940a158d064
LuposX <schuppsimon5@gmail.com>
2023-01-05 20:17:22 +0100

    Added example pythondoc for one class, changed README.md

--- pythoncode/README.md
+++ pythoncode/README.md
@@ -4,7 +4,24 @@ This subfolder contains the code for the project, for more information about the
 code check the "Documentation" or the "Entwurfsheft", for more information about
 the project check the README.md in the parent folder.
 
-## Usage
+
+## Development
+
+For the develop enviroment you need to have:
+- conda
+- git
+
+How to set up the develoment enviroment:
+1. `git clone https://github.com/LuposX/KonfiguratorFuerOSMDaten.git`
+2. `git checkout dev`
+3. switch to dev branch or your feature branch with: `git branch`
+4. Go to the folder `pythoncode`
+5. install requirements.yml via `conda env create --file requirement.yml`
+6. start your favourite IDE open the project and start coding :)
+
+For how to write correct documentation, check [this out](https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html).
+
+## Documentation
 
 To build the Documentation from the source code, execute the following commands:
 ```sh
---
+++ pythoncode/modules/model/CalculationManager.py
@@ -0,0 +1,41 @@
+from modules.model import CalculationPhase
+from modules.model import ConfigurationManager
+
+
+class CalculationManager:
+    """
+    The CalculationManager manages Calculation of the Project.
+    """
+
+    def __int__(self, starting_point: CalculationPhase, configuration_manager: ConfigurationManager):
+        """
+        Gets called when we first create an object of this class, it saves all information it needs for
+        starting a calculation.
+
+        Args:
+            starting_point: Describes in which calculation-phase we want to start the calculation.
+            configuration_manager: Saves all information required to configure the calculation.
+        """
+        pass
+
+    def cancel_calculation(self):
+        """
+        This method is used when we want to cancel an ongoing calculation.
+        """
+        pass
+
+    def _validate_starting_point(self) -> bool:
+        """
+        Validates the correctness of the Staring Point.
+
+        Returns:
+            bool: If true then the starting_point is valid, this means every calculation up to this point exist
+            and are saved in the project.
+        """
+        pass
+
+    def _start_calculation(self):
+        """
+        Starts the calculation.
+        """
+        pass
