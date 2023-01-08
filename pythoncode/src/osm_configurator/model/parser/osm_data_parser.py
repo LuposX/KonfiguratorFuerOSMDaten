9592d397c195449b52f1da024ea0ab3f2b71f33d
LuposX <schuppsimon5@gmail.com>
2023-01-08 15:48:57 +0100

    added new test_file for downloading data

---
+++ pythoncode/src/osm_configurator/model/parser/osm_data_parser.py
@@ -0,0 +1,14 @@
+from src.osm_configurator.model.parser.osm_data_parser_interface import OSMDataParserInterface
+
+
+class CategoryParser(OSMDataParserInterface):
+    __doc__ = OSMDataParserInterface.__doc__
+
+    def __init__(self):
+        """
+        Creates a new instance of the CategoryParser.
+        """
+        pass
+
+    def parse_osm_data_file(self, path):
+        pass
