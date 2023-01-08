af5a263d67090a8244ea58765929be28acab434d
LuposX <schuppsimon5@gmail.com>
2023-01-08 16:02:50 +0100

    more documentation of parser

--- pythoncode/src/osm_configurator/model/parser/cutOut_parser_interface.py
+++ pythoncode/src/osm_configurator/model/parser/cutOut_parser_interface.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from osm_configurator.model.project.calculation.traffic_cell import TrafficCell
+from src.osm_configurator.model.project.calculation.traffic_cell import TrafficCell
 
 
 class CutOutParserInterface(ABC):
--- pythoncode/src/osm_configurator/model/parser/osm_data_parser_interface.py
+++ pythoncode/src/osm_configurator/model/parser/osm_data_parser_interface.py
@@ -12,9 +12,10 @@ class OSMDataParserInterface(ABC):
     @abstractmethod
     def parse_osm_data_file(self, path):
         """
-        It gets an path toward an OSM data in Protocolbuffer Binary Format(pbf) and transforms it into an GeoDataFrame.
+        It gets a path toward an OSM data in Protocolbuffer Binary Format(pbf) and transforms it into an GeoDataFrame.
         Each row in the GeoDataFrame is a single data entry, which is an osm element from the read osm data.
-        Each column in the GeoDataFrame is a feature of the osm element from the osm_data, such as the location of the osm element.        
+        Each column in the GeoDataFrame is a feature of the osm element from the osm_data, such as the location of the
+        osm element, whereby a feature is a tag or something otherwise that describes the osm-element e.g. location.
 
         Args:
             path (pathlib.Path):  The path pointing towards the OSM data we want to parse, in the ".pbf" format.
