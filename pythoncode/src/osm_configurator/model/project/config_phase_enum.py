9d8e0b058416fd5b4a66c0d6204b176941b7a010
jphan <ujrlh@student.kit.edu>
2023-01-09 22:43:44 +0100

    Added cut_out_mode_enum.py und config_phase_enum.py

---
+++ pythoncode/src/osm_configurator/model/project/config_phase_enum.py
@@ -0,0 +1,25 @@
+from enum import Enum, unique
+
+def _data_config_phase():
+    pass
+
+
+def _category_config_phase():
+    pass
+
+def _reduction_config_phase():
+    pass
+
+def _attractivity_config_phase():
+    pass
+
+def _aggregation_config_phase():
+    pass
+
+def _calculation_config_phase():
+    pass
+
+
+@unique
+class ConfigPhase(Enum):
+    pass
---
+++ pythoncode/src/osm_configurator/model/project/configuration/cut_out_mode_enum.py
@@ -0,0 +1,12 @@
+from enum import Enum, unique
+
+def _partial_coverage_accepted():
+    pass
+
+
+def _total_coverage_requiered():
+    pass
+
+@unique
+class AggregationMethod(Enum):
+    pass
