d0808756f4f7b512ce2d1335019ccc1ae6ad4661
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-09 17:07:46 +0100

    updated docs of CalculationState

--- pythoncode/src/osm_configurator/model/project/calculation/calculation_state_enum.py
+++ pythoncode/src/osm_configurator/model/project/calculation/calculation_state_enum.py
@@ -6,13 +6,21 @@ class CalculationState(Enum):
     The states can be positive, indicating a correct working of the calculation. But they can be negative aswell, indicating an error in the calculations. Every state is defined by a unique description of the state.
     """
 
-    NOT_STARTED_YET = "The calculation was not started yet"
-    RUNNING = "The calculations are currently running"
-    ENDED_SUCCESSFULLY = "The calculations ended successfully"
-    ERROR_INVALID_OSM_DATA = "Error, the osm data are not valid"
-    ERROR_INVALID_CUT_OUT_DATA = "Error, the cut out data are not valid"
-    ERROR_INVALID_CATEGORIES = "Error, the category configuration is not valid"
-    ERROR_INVALID_PREVIOUS_CALCULATIONS = "Error, this calculation phase can not be calculated, because a previous calculation has invalid results or wasn't run"
+    NOT_STARTED_YET = ("Not started yet","The calculation was not started yet")
+    RUNNING = ("Running", "The calculations are currently running")
+    ENDED_SUCCESSFULLY = ("Done", "The calculations ended successfully")
+    ERROR_INVALID_OSM_DATA = ("Invalid OSM Data", "Error, the osm data are not valid")
+    ERROR_INVALID_CUT_OUT_DATA = ("Invalid Cut Out Data", "Error, the cut out data are not valid")
+    ERROR_INVALID_CATEGORIES = ("Invalid Categories", "Error, the category configuration is not valid")
+    ERROR_INVALID_PREVIOUS_CALCULATIONS = ("Invalid calculation phase", "Error, this calculation phase can not be calculated, because a previous calculation has invalid results or wasn't run")
+
+    def get_name(self):
+        """Gives the name of an calculation state.
+
+        Returns:
+            str: The name of the calculation satte
+        """
+        return self.value[0]
 
     def get_description(self):
         """Gives the description of an calculation state.
@@ -20,4 +28,4 @@ class CalculationState(Enum):
         Returns:
             str: The description, that describes the state in natural language.
         """
-        return self.value
+        return self.value[1]
