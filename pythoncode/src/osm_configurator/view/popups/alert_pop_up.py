2ed55431e278848503b730c09901738704c7cf4f
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-11 18:24:04 +0100

    added superclass of popups

--- pythoncode/src/osm_configurator/view/popups/alert_pop_up.py
+++ pythoncode/src/osm_configurator/view/popups/alert_pop_up.py
@@ -1,4 +1,7 @@
-class AlertPopUp:
+from customtkinter import CTkToplevel
+
+
+class AlertPopUp(CTkToplevel):
     """
     This class creates PopUps, that will pop up in front of the GUI.
     This instance is an Alert-PopUp. It provides a message and one 'OK' Button, to close the PopUp again.
