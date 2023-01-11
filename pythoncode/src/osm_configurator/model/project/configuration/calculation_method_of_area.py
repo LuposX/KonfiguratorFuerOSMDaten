7516ca72c27099f9094dce266815c4a62bb56e5e
LuposX <schuppsimon5@gmail.com>
2023-01-11 21:32:55 +0100

    rework of attractivity_attribute.py

--- pythoncode/src/osm_configurator/model/project/configuration/calculation_method_of_area.py
+++ pythoncode/src/osm_configurator/model/project/configuration/calculation_method_of_area.py
@@ -5,11 +5,18 @@ from enum import Enum, unique
 @unique
 class CalculationMethodOfArea(Enum):
     """
-    Enum Provides Calculation Method of the Area
+    Enum Provides Calculation Method of the Area.
     """
     CALCULATE_SITE_AREA = "Calculate Site Area"
     CALCULATE_BUILDING_AREA = "Calculate Building Area"
 
     @abstractmethod
     def get_calculation_method(self):
+        """
+        Getter for the name of the Calculation Method.
+
+        Returns:
+            str: The name of the Calculation Method.
+        """
         pass
+
