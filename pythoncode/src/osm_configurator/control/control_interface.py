428967c4f3b4873fc880069181dfe3d9a9eebb30
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-12 19:52:35 +0100

    fixed imports

--- pythoncode/src/osm_configurator/control/control_interface.py
+++ pythoncode/src/osm_configurator/control/control_interface.py
@@ -528,6 +528,6 @@ class IControl(ABC):
         Generates a graphic that visualizes the results of the calculations of the currently selected project.
 
         Returns:
-            matplotlib.Axes: The resulting visualization as axes of the matplotlib library.
+            matplotlib.axes.Axes: The resulting visualization as axes of the matplotlib library.
         """
         pass
