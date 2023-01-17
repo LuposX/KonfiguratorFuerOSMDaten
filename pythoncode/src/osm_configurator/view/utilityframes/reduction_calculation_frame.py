f32228be7cd4776f5111aeb6fb84b1563c7acf6c
LuposX <schuppsimon5@gmail.com>
2023-01-17 23:33:52 +0100

    refactored reduction_calculation_frame.py

--- pythoncode/src/osm_configurator/view/utilityframes/reduction_calculation_frame.py
+++ pythoncode/src/osm_configurator/view/utilityframes/reduction_calculation_frame.py
@@ -1,20 +1,23 @@
 import src.osm_configurator.view.toplevelframes.reduction_frame
+import src.osm_configurator.control.category_controller
+
+from src.osm_configurator.view.activatable import Activatable
 
 
-class ReductionCalculationFrame:
+class ReductionCalculationFrame(Activatable):
     """
     This frame provides the ability to the user to set how the calculation of the reduction of a category
     will be done.
     This is a subframe from the ReductionFrame.
     """
 
-    def __init__(self, parent, control):
+    def __init__(self, parent, category_controller):
         """
         This method creates a ReductionCalculationFrame that lets the user edit the calculation of the reduction of
         Categories.
         Args:
             parent (reduction_frame.ReductionFrame): This is the parent frame of this frame. The frame will be located here.
-            control (control_interface.IControl): The control the frame will call to get access to the model.
+            category_controller (category_controller.CategoryController): Respective controller.
         """
         pass
 
