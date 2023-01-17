1929303bdb1656b2e2c57408ae943de7a641f573
LuposX <schuppsimon5@gmail.com>
2023-01-17 23:22:18 +0100

    refactored create_project_frame.py

--- pythoncode/src/osm_configurator/view/toplevelframes/create_project_frame.py
+++ pythoncode/src/osm_configurator/view/toplevelframes/create_project_frame.py
@@ -1,4 +1,6 @@
 import src.osm_configurator.view.states.state_manager
+import src.osm_configurator.control.project_controller
+
 from src.osm_configurator.view.toplevelframes.top_level_frame import TopLevelFrame
 
 
@@ -9,13 +11,13 @@ class CreateProjectFrame(TopLevelFrame):
     The user can cancel the creation-process.
     """
 
-    def __init__(self, state_manager, control):
+    def __init__(self, state_manager, project_controller):
         """
         This method creates a CreateProjectFrame where a user can create a new project.
 
         Args:
             state_manager (state_manager.StateManager): The StateManager the frame will call, if it wants to change to another State.
-            control (control_interface.IControl): The frame will call the control to gain access to the model.
+            project_controller (project_controller.ProjectController): Respective controller
         """
         super().__init__(state_manager, control)
         pass
