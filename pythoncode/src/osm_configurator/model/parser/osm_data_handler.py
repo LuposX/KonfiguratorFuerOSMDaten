191d3581a3d377f04458cb0b90d706920e42d21c
LuposX <schuppsimon5@gmail.com>
2023-01-21 12:58:30 +0100

    finished osm_data_handler.py

--- pythoncode/src/osm_configurator/model/parser/osm_data_handler.py
+++ pythoncode/src/osm_configurator/model/parser/osm_data_handler.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import List
+    from typing import Tuple
     from src.osm_configurator.model.project.configuration.category_manager import CategoryManager
     from src.osm_configurator.model.project.configuration.category import Category
     from src.osm_configurator.model.project.configuration.attribute_enum import Attribute
@@ -31,16 +32,16 @@ class DataOSMHandler(osm.SimpleHandler):
         """
         osm.SimpleHandler.__init__(self)
 
-        # This will be the list in which we save the output
+        # This will be the list in which we save the output.
         self._osm_data: List = []
 
-        # this is a temporary list that is used to save the tags for one osm element
+        # this is a temporary list that is used to save the tags for one osm element.
         self._tmp_tag_list: List = []
 
         self._category_manager: CategoryManager = categories
         self._activated_attributes: List = activated_attributes
 
-        # Get a list of tags that are needed, the rest we can throw away
+        # Get a list of tags that are needed, the rest we can throw away.
         self._needed_tags: List = self._attributes_to_tag_list()
 
     def _attributes_to_tag_list(self) -> List:
@@ -67,15 +68,63 @@ class DataOSMHandler(osm.SimpleHandler):
         # Get a temporary list of tags from the osm object.
         self._tmp_tag_list: List = []
         for tag in elem.tags:
-            # only save the tags if we need them later
+            # only save the tags if we need them later.
             if tag.k in self._needed_tags:
                 self._tmp_tag_list.append((tag.k, tag.v))
 
-        # save the osm object data that we need
+        # save the osm object data that we need.
         self._osm_data.append([elem_type,
                                len(self._tmp_tag_list),
                                np.asarray(self._tmp_tag_list, dtype=str),
                                np.asarray(categories_of_osm_element, dtype=np.uint16)])
+
+    def _get_list_of_categories_of_the_osm_element(self, n: OSMObject) -> List[str]:
+        """
+        This class checks for a specific osm_element which categories apply to it.
+
+        Args:
+            n (OSMObject): The  osm object which we want to check.
+
+        Returns:
+            List[str]: A list of categories that apply to the osm element.
+        """
+        _categories_of_osm_element = []
+
+        # check if the osm_element applies to a category.
+        _category: Category
+        for _category in self._category_manager.get_categories():
+            _category_name = _category.get_category_name()
+            _whitelist = _category.get_whitelist()
+            _blacklist = _category.get_whitelist()
+
+            # check if the node adheres to the whitelist.
+            _all_tags_from_whitelist_correct: bool = True
+            _tag_in_whitelist: Tuple
+            for _tag_in_whitelist in _whitelist:
+
+                # If we find a single tag from the whitelist which the node doesn't correctly have, don't add category.
+                if n.tags.get(_tag_in_whitelist[0]) != _tag_in_whitelist[1]:
+                    _all_tags_from_whitelist_correct = False
+                    break
+
+            # Only when the whitelist is correct do we need to check the blacklist.
+            if _all_tags_from_whitelist_correct:
+
+                # check if the node adheres to the blacklist.
+                _all_tags_from_blacklist_correct: bool = True
+                _tag_in_blacklist: Tuple
+                for _tag_in_blacklist in _blacklist:
+
+                    # If we find a single tag from the blacklist which the node doesn't adhere to,
+                    # then the category doesn't apply to the osm_element.
+                    if n.tags.get(_tag_in_blacklist[0]) == _tag_in_blacklist[1]:
+                        _all_tags_from_whitelist_correct = False
+                        break
+
+            if _all_tags_from_whitelist_correct:
+                _categories_of_osm_element.append(_category_name)
+
+        return _categories_of_osm_element
 
     def node(self, n: Node) -> None:
         """
@@ -84,42 +133,30 @@ class DataOSMHandler(osm.SimpleHandler):
         Args:
             n (Node): The node we found
         """
-        categories_of_osm_element: List = []
+        # Get all the categories that apply to the current osm element
+        _categories_of_osm_element = self._get_list_of_categories_of_the_osm_element(n)
 
-        # check if the osm_element applies to a category
-        _category: Category
-        for _category in self._category_manager.get_categories():
-            _name = _category.get_category_name()
-            _whitelist = _category.get_whitelist()
-            _blacklist = _category.get_whitelist()
-
-            if n.tags.get(whitelist[0]) == whitelist[1]:
-                categories_of_osm_element.append(name)
-
-        if categories_of_osm_element:
-            self._tag_inventory(n, "node", categories_of_osm_element)
+        # check that the categories aren't empty
+        if _categories_of_osm_element:
+            self._tag_inventory(n, "node", _categories_of_osm_element)
 
         del n
 
     def way(self, w: Way) -> None:
         """
-          This method gets called when reading the osm data file a way was found.
+        This method gets called when reading the osm data file a way was found.
 
-          Args:
-              w (Way): The way we found.
-          """
-        categories_of_osm_element = []
+        Args:
+            w (Way): The way we found.
+        """
+        # Get all the categories that apply to the current osm element
+        _categories_of_osm_element = self._get_list_of_categories_of_the_osm_element(w)
 
-        # check if the osm_element applies to a category
-        for category in self.category_whitelist:
-            name = category[0]
-            whitelist = category[1]
+        # check that the categories aren't empty
+        if _categories_of_osm_element:
+            self._tag_inventory(w, "way", _categories_of_osm_element)
 
-            if w.tags.get(whitelist[0]) == whitelist[1]:
-                categories_of_osm_element.append(name)
-
-        if categories_of_osm_element:
-            self._tag_inventory(w, "way", categories_of_osm_element)
+        del w
 
     def relation(self, r: Relation) -> None:
         """
@@ -128,16 +165,11 @@ class DataOSMHandler(osm.SimpleHandler):
         Args:
             r (Relation): The node we found
         """
-        # It's really hard to get the location out of relations
-        categories_of_osm_element = []
+        # Get all the categories that apply to the current osm element
+        _categories_of_osm_element = self._get_list_of_categories_of_the_osm_element(r)
 
-        # check if the osm_element applies to a category
-        for category in self.category_whitelist:
-            name = category[0]
-            whitelist = category[1]
+        # check that the categories aren't empty
+        if _categories_of_osm_element:
+            self._tag_inventory(r, "relation", _categories_of_osm_element)
 
-            if r.tags.get(whitelist[0]) == whitelist[1]:
-                categories_of_osm_element.append(name)
-
-        if categories_of_osm_element:
-            self._tag_inventory(r, "relation", categories_of_osm_element)
+        del r
