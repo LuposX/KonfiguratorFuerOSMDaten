8552d332b0494c888f87c29f0741382a4bf0746c
LuposX <schuppsimon5@gmail.com>
2023-01-21 13:21:41 +0100

    correct signature for data parser

--- pythoncode/src/osm_configurator/model/parser/osm_data_parser.py
+++ pythoncode/src/osm_configurator/model/parser/osm_data_parser.py
@@ -1,4 +1,18 @@
+from __future__ import annotations
+
+import geopandas as gpd
+
+import osm_configurator.model.parser.osm_data_handler as osm_data_handler
 from src.osm_configurator.model.parser.osm_data_parser_interface import OSMDataParserInterface
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List
+    from src.osm_configurator.model.project.configuration.category_manager import CategoryManager
+    from src.osm_configurator.model.project.configuration.attribute_enum import Attribute
+    from osm_configurator.model.parser.osm_data_handler import DataOSMHandler
+    from geopandas import GeoDataFrame
 
 
 class OSMDataParser(OSMDataParserInterface):
@@ -10,7 +24,18 @@ class OSMDataParser(OSMDataParserInterface):
         """
         pass
 
-    def parse_osm_data_file(self, path):
-        # opening the file
-        with open(path.absolute(), "r") as traffic_cell_file:
+    # TODO: wrong signature fix this
+    def parse_osm_data_file(self, path, categories: CategoryManager, activated_attributes: List[Attribute]) -> GeoDataFrame:
+        # Creating our osm_handler which converts the osm-data file into a list of data
+        osm_handler: DataOSMHandler = osm_data_handler.DataOSMHandler(categories, activated_attributes)
+
+        # Process the data
+        osm_handler.apply_file(path.absolute())
+
+        # transform the data from osm_handler into  geoDataFrame
+        data_col_names = ['osm_type', 'ntags', 'tags', 'categories']
+        df_osm = gpd.GeoDataFrame(osm_handler.get_osm_data(), columns=data_col_names)
+
+        return df_osm
+
 
