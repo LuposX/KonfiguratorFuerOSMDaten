4c1b4fcbfc9cf30073c36ee226c09ffdecfffe17
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-21 21:09:24 +0100

    Added SplitUpFiles and it's tests.

--- pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
+++ pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
+import subprocess
+import os
 import shapely
 import pathlib
+import subprocess
+import shapely.geometry
+import geopandas as gpd
 
 
 class SplitUpFile:
@@ -10,23 +15,35 @@ class SplitUpFile:
     This is useful since an osm-data file loaded into the ram can be bigger than the capacity of the RAM.
     """
 
-    def __init__(self, file_path):
+    def __init__(self, origin_path, result_folder):
         """
         Creates a new instance of "SplitUpFile".
 
         Args:
-            file_path (pathlib.Path): The path pointing towards the osm_data file we want to split.
+            origin_path (pathlib.Path): The path pointing towards the osm_data file we want to split.
+            result_folder (pathlib.Path): The path pointing towards the folder, where we want the split up files to land
         """
-        pass
+        self.origin_path = origin_path
+        self.result_folder = result_folder
 
     def split_up_files(self, coordinates):
         """
         This method splits up the file into multiple smaller ones based on the coordinates it receives.
 
         Args:
-            coordinates (List[shapely.Polygon]): A list of polygon. Each polygon ist the bounding box of one traffic cell we want to split up.
+            coordinates (List[geopandas.geoseries.GeoSeries]): A list of polygon. Each polygon ist the bounding box of one traffic cell we want to split up.
 
         Returns:
             bool: True if successful, otherwise false.
         """
-        pass
+
+        if not os.path.exists(self.origin_path) or not os.path.exists(self.result_folder):
+            return False
+
+        for i in range(len(coordinates)):
+            result = subprocess.run(["osmium", "extract", "-b", "{},{},{},{}".format(*coordinates[i].bounds),
+                                     str(self.origin_path), "-o", str(self.result_folder) + "/" + str(i) + ".pbf"],
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            if result.returncode != 0:
+                return False
+        return True
---
+++ pythoncode/tests/unittests/osm_configurator/model/project/calculation/test_split_up_files.py
@@ -0,0 +1,58 @@
+import os.path
+
+import geopandas as gpd
+from pathlib import Path
+import src.osm_configurator.model.project.calculation.split_up_files as suf
+
+
+def test_illegal_origin_path():
+    origin_path = Path("adhadhkaduad/ishlisafglaif.pbf")
+    result_folder = Path("../../../../../build/split_up_files")
+    geojson_path = Path("../../../../../data/monaco-regions.geojson")
+
+    df = gpd.read_file(geojson_path)
+    split_up = suf.SplitUpFile(origin_path, result_folder)
+
+    did_work = split_up.split_up_files(df["geometry"])
+    assert not did_work
+
+
+def test_illegal_result_folder():
+    origin_path = Path("../../../../../data/monaco-latest.osm.pbf")
+    result_folder = Path("asdiodoadaos/asdhidhaodh")
+    geojson_path = Path("../../../../../data/monaco-regions.geojson")
+
+    df = gpd.read_file(geojson_path)
+    split_up = suf.SplitUpFile(origin_path, result_folder)
+
+    did_work = split_up.split_up_files(df["geometry"])
+    assert not did_work
+
+
+def test_correct_split_up():
+    # Declare paths
+    origin_path = Path("../../../../../data/monaco-latest.osm.pbf")
+    result_folder = Path("../../../../../build/split_up_files")
+    geojson_path = Path("../../../../../data/monaco-regions.geojson")
+    test_file_path = Path("../../../../../build/split_up_files/3.pbf")
+
+    # Clean up last tests
+    # Create build directory
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+
+    # Delete old build files
+    delete_path = str(result_folder) + '//'
+    for file_name in os.listdir(delete_path):
+        # construct full file path
+        file = delete_path + file_name
+        if os.path.isfile(file):
+            os.remove(file)
+
+    # Begin test
+    df = gpd.read_file(geojson_path)
+    split_up = suf.SplitUpFile(origin_path, result_folder)
+
+    did_work = split_up.split_up_files(df["geometry"])
+    assert did_work
+    assert os.path.exists(test_file_path)
