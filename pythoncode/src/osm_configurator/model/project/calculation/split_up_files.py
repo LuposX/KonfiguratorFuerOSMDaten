cacce43467dc102369d4503a8e6e8afd1cdbf255
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-22 04:50:30 +0100

    changed up split_up_files to support names of cells.

--- pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
+++ pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
@@ -26,12 +26,15 @@ class SplitUpFile:
         self.origin_path = origin_path
         self.result_folder = result_folder
 
-    def split_up_files(self, coordinates):
+    def split_up_files(self, cells):
         """
         This method splits up the file into multiple smaller ones based on the coordinates it receives.
+        The split up is based on a DataFrame. The dataframe must contain a row for each splitting. It must have the
+        column geometry, which contains a GeoSeries and a column name, which contains the name of the file of the
+        split up.
 
         Args:
-            coordinates (List[geopandas.geoseries.GeoSeries]): A list of polygon. Each polygon ist the bounding box of one traffic cell we want to split up.
+            cells (geopandas.geodataframe.GeoDataFrame): The above-mentioned data frame
 
         Returns:
             bool: True if successful, otherwise false.
@@ -40,9 +43,13 @@ class SplitUpFile:
         if not os.path.exists(self.origin_path) or not os.path.exists(self.result_folder):
             return False
 
-        for i in range(len(coordinates)):
-            result = subprocess.run(["osmium", "extract", "-b", "{},{},{},{}".format(*coordinates[i].bounds),
-                                     str(self.origin_path), "-o", str(self.result_folder) + "/" + str(i) + ".pbf"],
+        if not 'name' in cells.columns:
+            return False
+
+        for i in range(len(cells["geometry"])):
+            result = subprocess.run(["osmium", "extract", "-b", "{},{},{},{}".format(*cells["geometry"][i].bounds),
+                                     str(self.origin_path), "-o",
+                                     str(self.result_folder) + "/" + str(cells["name"][i]) + ".pbf"],
                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if result.returncode != 0:
                 return False
--- pythoncode/tests/unittests/osm_configurator/model/project/calculation/test_split_up_files.py
+++ pythoncode/tests/unittests/osm_configurator/model/project/calculation/test_split_up_files.py
@@ -29,7 +29,7 @@ def test_illegal_result_folder():
     assert not did_work
 
 
-def test_correct_split_up():
+def test_no_name_column():
     # Declare paths
     origin_path = Path("../../../../../data/monaco-latest.osm.pbf")
     result_folder = Path("../../../../../build/split_up_files")
@@ -53,6 +53,38 @@ def test_correct_split_up():
     df = gpd.read_file(geojson_path)
     split_up = suf.SplitUpFile(origin_path, result_folder)
 
-    did_work = split_up.split_up_files(df["geometry"])
+    did_work = split_up.split_up_files(df)
+    assert not did_work
+
+
+def test_correct_split_up():
+    # Declare paths
+    origin_path = Path("../../../../../data/monaco-latest.osm.pbf")
+    result_folder = Path("../../../../../build/split_up_files")
+    geojson_path = Path("../../../../../data/monaco-regions.geojson")
+    test_file_path1 = Path("../../../../../build/split_up_files/3.pbf")
+    test_file_path2 = Path("../../../../../build/split_up_files/cell_with_a_name.pbf")
+
+    # Clean up last tests
+    # Create build directory
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+
+    # Delete old build files
+    delete_path = str(result_folder) + '//'
+    for file_name in os.listdir(delete_path):
+        # construct full file path
+        file = delete_path + file_name
+        if os.path.isfile(file):
+            os.remove(file)
+
+    # Begin test
+    df = gpd.read_file(geojson_path)
+    df["name"] = df.index
+    df["name"][2] = "cell_with_a_name"
+    split_up = suf.SplitUpFile(origin_path, result_folder)
+
+    did_work = split_up.split_up_files(df)
     assert did_work
-    assert os.path.exists(test_file_path)
+    assert os.path.exists(test_file_path1)
+    assert os.path.exists(test_file_path2)
