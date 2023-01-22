1aabd6d0a6eafcca4c383ea7764ddc8c8ff47db0
LuposX <schuppsimon5@gmail.com>
2023-01-22 14:57:10 +0100

    added pydoc to utility function

--- pythoncode/src/osm_configurator/model/project/calculation/calculation_phase_utility.py
+++ pythoncode/src/osm_configurator/model/project/calculation/calculation_phase_utility.py
@@ -8,7 +8,18 @@ if TYPE_CHECKING:
     from pathlib import Path
 
 
-def get_checkpoints_folder_path_from_phase(configuration_manager_o: ConfigurationManager, phase: CalculationPhase):
+def get_checkpoints_folder_path_from_phase(configuration_manager_o: ConfigurationManager, phase: CalculationPhase) -> Path:
+    """
+    This method creates the full path which points toward the results/intermediate-step folder of a certain phase.
+    It combines the path of the active_project with the result folder name and the phase result folder name.
+
+    Args:
+        configuration_manager_o (ConfigurationManager): Is used to find out the path towards the folder.
+        phase (CalculationPhase): The phase from which we want to get the results/intermediate-step folder.
+
+    Returns:
+        (Path): The full path towards the desired calculation phase results folder.
+    """
     # Get the path to the project path and the name of the folder where we save the results and add them together
     project_path: Path = configuration_manager_o.get_active_project_path()
 
