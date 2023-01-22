cdf3368eb8ad286a509a0b624867787f111df1db
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-22 11:17:21 +0100

    fixed test

--- pythoncode/tests/unittests/osm_configurator/model/project/calculation/test_osm_file_format_enum.py
+++ pythoncode/tests/unittests/osm_configurator/model/project/calculation/test_osm_file_format_enum.py
@@ -3,5 +3,5 @@ import src.osm_configurator.model.project.calculation.osm_file_format_enum as fo
 
 def test_get_file_extension():
     assert format.OSMFileFormat.PBF.get_file_extension() == ".pbf"
-    assert format.OSMFileFormat.BZ2.get_file_extension() == ".osm.bz2"
+    assert format.OSMFileFormat.BZ2.get_file_extension() == ".bz2"
     assert format.OSMFileFormat.OSM.get_file_extension() == ".osm"
