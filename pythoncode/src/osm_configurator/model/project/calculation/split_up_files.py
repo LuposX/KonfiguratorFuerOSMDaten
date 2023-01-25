ae7d9f9e5f6f5ee5482179db54d7412e7dc6b91b
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-25 15:39:19 +0100

    added typehints

--- pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
+++ pythoncode/src/osm_configurator/model/project/calculation/split_up_files.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 import subprocess
 import os
 import shapely
-import pathlib
+from pathlib import Path
 import subprocess
 import shapely.geometry
 import geopandas as gpd
+from geopandas.geodataframe import GeoDataFrame
 
 
 class SplitUpFile:
@@ -15,7 +16,7 @@ class SplitUpFile:
     This is useful since an osm-data file loaded into the ram can be bigger than the capacity of the RAM.
     """
 
-    def __init__(self, origin_path, result_folder):
+    def __init__(self, origin_path: Path, result_folder: Path):
         """
         Creates a new instance of "SplitUpFile".
 
@@ -23,10 +24,10 @@ class SplitUpFile:
             origin_path (pathlib.Path): The path pointing towards the osm_data file we want to split.
             result_folder (pathlib.Path): The path pointing towards the folder, where we want the split up files to land
         """
-        self.origin_path = origin_path
-        self.result_folder = result_folder
+        self._origin_path: Path = origin_path
+        self._result_folder: Path = result_folder
 
-    def split_up_files(self, cells):
+    def split_up_files(self, cells: GeoDataFrame) -> bool:
         """
         This method splits up the file into multiple smaller ones based on the coordinates it receives.
         The split up is based on a DataFrame. The dataframe must contain a row for each splitting. It must have the
@@ -34,22 +35,22 @@ class SplitUpFile:
         split up.
 
         Args:
-            cells (geopandas.geodataframe.GeoDataFrame): The above-mentioned data frame
+            cells (GeoDataFrame): The above-mentioned data frame
 
         Returns:
             bool: True if successful, otherwise false.
         """
 
-        if not os.path.exists(self.origin_path) or not os.path.exists(self.result_folder):
+        if not os.path.exists(self._origin_path) or not os.path.exists(self._result_folder):
             return False
 
-        if not 'name' in cells.columns:
+        if 'name' not in cells.columns:
             return False
 
         for i in range(len(cells["geometry"])):
             result = subprocess.run(["osmium", "extract", "-b", "{},{},{},{}".format(*cells["geometry"][i].bounds),
-                                     str(self.origin_path), "-o",
-                                     str(self.result_folder) + "/" + str(cells["name"][i]) + ".pbf"],
+                                     str(self._origin_path), "-o",
+                                     str(self._result_folder) + "/" + str(cells["name"][i]) + ".pbf"],
                                     stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             if result.returncode != 0:
                 return False
