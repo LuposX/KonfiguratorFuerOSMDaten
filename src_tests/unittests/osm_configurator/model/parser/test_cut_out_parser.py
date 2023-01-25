01e13b22916184bcfc9051e389f94bac3e8427af
LuposX <schuppsimon5@gmail.com>
2023-01-25 19:24:01 +0100

    more tests for cutout parser

--- src_tests/unittests/osm_configurator/model/parser/test_cut_out_parser.py
+++ src_tests/unittests/osm_configurator/model/parser/test_cut_out_parser.py
@@ -1,10 +1,15 @@
 import os
 
 import src.osm_configurator.model.parser.cut_out_parser as cop
-from src_tests.definitions import TEST_DIR
+from src_tests.definitions import TEST_DIR, MONACO_TRAFFIC_CELL_0_POLYGON, MONACO_TRAFFIC_CELL_1_POLYGON
 from src.osm_configurator.model.parser import dataframe_column_names
 
 from pathlib import Path
+import shapely as shp
+
+
+# without this you get a weird error, idk why
+os.environ["PROJ_LIB"]=""
 
 
 def test_correct_parsing():
@@ -12,5 +17,15 @@ def test_correct_parsing():
     parser = cop.CutOutParser()
     df = parser.parse_cutout_file(Path(geojson_path))
 
+    # Test non generated names
+    assert df[dataframe_column_names.TRAFFIC_CELL_NAME][0] == "0_super_traffic_cell"
+    assert df[dataframe_column_names.TRAFFIC_CELL_NAME][1] == "1_the_funny_cat"
+
+    # Test auto generated names
+    assert df[dataframe_column_names.TRAFFIC_CELL_NAME][2] == "2_traffic_cell"
     assert df[dataframe_column_names.TRAFFIC_CELL_NAME][3] == "3_traffic_cell"
+
+    # Test parsed geometry
+    assert MONACO_TRAFFIC_CELL_0_POLYGON == df[dataframe_column_names.GEOMETRY][0]
+    assert MONACO_TRAFFIC_CELL_1_POLYGON == df[dataframe_column_names.GEOMETRY][1]
     assert "7.430574755831088 43.74116219248498" in str(df[dataframe_column_names.GEOMETRY][5])
