8038ab2a7f25a06df1e41c6ae2e35a7d9fd50635
LuposX <schuppsimon5@gmail.com>
2023-01-27 15:49:07 +0100

    added the path towards the cplit up files, files have wron name

--- src_tests/unittests/osm_configurator/model/parser/teste_osm_data_handler.py
+++ src_tests/unittests/osm_configurator/model/parser/teste_osm_data_handler.py
@@ -1,12 +1,12 @@
 import src.osm_configurator.model.parser.osm_data_handler as osm_data_handler_i
 
-from src_tests.definitions import CATEGORY_MANAGER, MONACO_TRAFFIC_CELL_1_POLYGON
+from src_tests.definitions import CATEGORY_MANAGER, MONACO_TRAFFIC_CELL_1_POLYGON, TEST_DIR
 
 
 def test_osm_data_handler():
     osm_data_handler = osm_data_handler_i.DataOSMHandler(CATEGORY_MANAGER, MONACO_TRAFFIC_CELL_1_POLYGON)
 
-    osm_data_handler.apply_file(PLACEHOLDER)
+    osm_data_handler.apply_file(TEST_DIR.join("data/monaco_split_up_files/0.pbf"))
 
     test = osm_data_handler.get_osm_data()
 
