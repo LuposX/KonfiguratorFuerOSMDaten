ed852fef7af4f33dcacbcec87e73e96c82a9b0c5
LuposX <schuppsimon5@gmail.com>
2023-01-27 16:02:30 +0100

    basic test should now be correct

--- src_tests/unittests/osm_configurator/model/parser/teste_osm_data_handler.py
+++ src_tests/unittests/osm_configurator/model/parser/teste_osm_data_handler.py
@@ -6,9 +6,17 @@ from src_tests.definitions import CATEGORY_MANAGER, MONACO_TRAFFIC_CELL_1_POLYGO
 def test_osm_data_handler():
     osm_data_handler = osm_data_handler_i.DataOSMHandler(CATEGORY_MANAGER, MONACO_TRAFFIC_CELL_1_POLYGON)
 
-    osm_data_handler.apply_file(TEST_DIR.join("data/monaco_split_up_files/0.pbf"))
+    osm_data_handler.apply_file(TEST_DIR.join("data/monaco_split_up_files/0_super_traffic_cell.pbf"))
 
     test_osm_data = osm_data_handler.get_osm_data()
 
     # Checks that the list is not empty
     assert test_osm_data
+
+    osm_data_handler.apply_file(TEST_DIR.join("data/monaco_split_up_files/1_the_funny_cat.pbf"))
+
+    test_osm_data = osm_data_handler.get_osm_data()
+
+    # Checks that the list is not empty
+    assert test_osm_data
+
