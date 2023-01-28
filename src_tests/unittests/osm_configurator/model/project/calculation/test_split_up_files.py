53773069e63f79ed27fb69cd0ec0e00bd293373a
Karl Bernhard <karl-bernhard1@gmx.de>
2023-01-28 20:42:04 +0100

    fixed merge errors

--- src_tests/unittests/osm_configurator/model/project/calculation/test_split_up_files.py
+++ src_tests/unittests/osm_configurator/model/project/calculation/test_split_up_files.py
@@ -4,7 +4,7 @@ from src_tests.definitions import TEST_DIR
 import geopandas as gpd
 from pathlib import Path
 import src.osm_configurator.model.project.calculation.split_up_files as suf
-from src.osm_configurator.model.parser import dataframe_column_names
+import src.osm_configurator.model.model_constants as model_constants
 
 
 def _prepare(result_folder):
@@ -29,7 +29,7 @@ def test_illegal_origin_path():
     df = gpd.read_file(geojson_path)
     split_up = suf.SplitUpFile(Path(origin_path), Path(result_folder))
 
-    did_work = split_up.split_up_files(df[dataframe_column_names.GEOMETRY])
+    did_work = split_up.split_up_files(df[model_constants.CL_LOCATION])
     assert not did_work
 
 
@@ -41,7 +41,7 @@ def test_illegal_result_folder():
     df = gpd.read_file(geojson_path)
     split_up = suf.SplitUpFile(Path(origin_path), Path(result_folder))
 
-    did_work = split_up.split_up_files(df[dataframe_column_names.GEOMETRY])
+    did_work = split_up.split_up_files(df[model_constants.CL_LOCATION])
     assert not did_work
 
 
@@ -74,8 +74,8 @@ def test_correct_split_up():
 
     # Begin test
     df = gpd.read_file(geojson_path)
-    df[dataframe_column_names.TRAFFIC_CELL_NAME] = df.index
-    df[dataframe_column_names.TRAFFIC_CELL_NAME][2] = "cell_with_a_name"
+    df[model_constants.CL_TRAFFIC_CELL_NAME] = df.index
+    df[model_constants.CL_TRAFFIC_CELL_NAME][2] = "cell_with_a_name"
     split_up = suf.SplitUpFile(Path(origin_path), Path(result_folder))
 
     did_work = split_up.split_up_files(df)
