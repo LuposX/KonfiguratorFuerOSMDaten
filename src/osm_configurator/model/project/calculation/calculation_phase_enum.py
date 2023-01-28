ca9012c76c495ee3204c74560eacd63d8f6a2835
LuposX <schuppsimon5@gmail.com>
2023-01-28 23:26:15 +0100

    removed mypy fro mworkflow

--- src/osm_configurator/model/project/calculation/calculation_phase_enum.py
+++ src/osm_configurator/model/project/calculation/calculation_phase_enum.py
@@ -40,7 +40,7 @@ class CalculationPhase(Enum):
         """
         return self.value[0]
 
-    def get_folder_name_for_results(self) -> str:
+    def get_folder_name_for_results(self):
         """
         Getter for the folder name of the enum type.
 
