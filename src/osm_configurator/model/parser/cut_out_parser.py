1aad2d11ffd8f2a333b1a0a047ae2ba29c73a3ab
LuposX <schuppsimon5@gmail.com>
2023-01-28 19:25:55 +0100

    removed old name for constant class

--- src/osm_configurator/model/parser/cut_out_parser.py
+++ src/osm_configurator/model/parser/cut_out_parser.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from src.osm_configurator.model.parser.cut_out_parser_interface import CutOutParserInterface
-import src.osm_configurator.model.model_constants as dataframe_column_names
+import src.osm_configurator.model.model_constants as model_constants_i
 
 import geopandas as gpd
 
@@ -30,16 +30,16 @@ class CutOutParser(CutOutParserInterface):
         # Create names for the traffic cells which don't have one and add idx before name
         # e.g. what function does "None" -> "0_traffic_cell" and "berlin_is_cool" -> "1_berlin_is_cool"
         traffic_cell_name_list: List = []
-        if dataframe_column_names.CL_TRAFFIC_CELL_NAME in df.columns:
+        if model_constants_i.CL_TRAFFIC_CELL_NAME in df.columns:
             for idx, row in df.iterrows():
-                if row[dataframe_column_names.CL_TRAFFIC_CELL_NAME] is None:
+                if row[model_constants_i.CL_TRAFFIC_CELL_NAME] is None:
                     traffic_cell_name_list.append(str(idx) + "_" + CutOutParser.TRAFFIC_CELL_STANDARD_NAME)
                 else:
-                    traffic_cell_name_list.append(str(idx) + "_" + str(row[dataframe_column_names.CL_TRAFFIC_CELL_NAME]))
+                    traffic_cell_name_list.append(str(idx) + "_" + str(row[model_constants_i.CL_TRAFFIC_CELL_NAME]))
         else:
             for idx, row in df.iterrows():
                 traffic_cell_name_list.append(str(idx) + "_" + CutOutParser.TRAFFIC_CELL_STANDARD_NAME)
 
-        df[dataframe_column_names.CL_TRAFFIC_CELL_NAME] = traffic_cell_name_list
+        df[model_constants_i.CL_TRAFFIC_CELL_NAME] = traffic_cell_name_list
 
         return df
