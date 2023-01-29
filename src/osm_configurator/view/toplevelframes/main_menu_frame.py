4a78417d0e1c3b5bfd818322d73424977aad21a4
uxhoe <weik.felix@gmx.de>
2023-01-29 21:26:15 +0100

    First implementation, very lost what should happen

--- src/osm_configurator/view/toplevelframes/main_menu_frame.py
+++ src/osm_configurator/view/toplevelframes/main_menu_frame.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
-import src.osm_configurator.view.states.state_manager
-import src.osm_configurator.control.project_controller_interface
+import src.osm_configurator.view.states.state_manager as state_manager
+import src.osm_configurator.control.project_controller_interface as project_controller_interface
 
 from src.osm_configurator.view.toplevelframes.top_level_frame import TopLevelFrame
+
+import customtkinter
 
 
 class MainMenuFrame(TopLevelFrame):
@@ -13,17 +15,28 @@ class MainMenuFrame(TopLevelFrame):
     will be shown in a list and can be selected / opened.
     """
 
-    def __init__(self, state_manager, project_controller):
+    def __init__(self, _state_manager: state_manager.StateManager, project_controller, control):
         """
         This method creates a MainMenuFrame showing the MainMenu of the application.
 
         Args:
-            state_manager (state_manager.StateManager): The frame will call the StateManager, if it wants to switch states.
+            _state_manager (state_manager.StateManager): The frame will call the StateManager, if it wants to switch states.
             project_controller (project_controller.ProjectController): Respective controller
             control (control_interface.IControl): The frame will call the control to gain access to the model.
         """
-        # super().__init__(state_manager, control)
-        pass
+        window = super().__init__(state_manager, control)
+
+        customtkinter.CTkButton(window, text="New Project") \
+            .pack(side="left", padx=40, pady=40)
+
+        customtkinter.CTkButton(window, text="Load external Project") \
+            .pack(side="left", padx=40, pady=40)
+
+        customtkinter.CTkButton(window, text="Load selected Project") \
+            .pack(side="left", padx=40, pady=40)
+
+        customtkinter.CTkButton(window, text="Settings") \
+            .pack(side="left", padx=40, pady=40)
 
     def activate(self):
         pass
