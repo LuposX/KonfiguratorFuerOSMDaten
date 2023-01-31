1c894a85487fb1b05b1f409de01d058120ee4359
uxhoe <weik.felix@gmx.de>
2023-01-31 19:54:05 +0100

    Added basic functionality and todos for my future-self

--- src/osm_configurator/view/toplevelframes/main_menu_frame.py
+++ src/osm_configurator/view/toplevelframes/main_menu_frame.py
@@ -9,6 +9,13 @@ import src.osm_configurator.view.states.state_name_enum as sne
 
 import customtkinter
 
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from src.osm_configurator.control.project_controller_interface import IProjectController
+    from src.osm_configurator.view.states.state_manager import StateManager
+    from src.osm_configurator.model.application.passive_project import PassiveProject
+
 
 class MainMenuFrame(Activatable, customtkinter.CTkToplevel):
     """
@@ -17,15 +24,21 @@ class MainMenuFrame(Activatable, customtkinter.CTkToplevel):
     will be shown in a list and can be selected / opened.
     """
 
-    def __init__(self, _state_manager: state_manager.StateManager, project_controller, control):
+    def __init__(self, state_manager: StateManager, project_controller: IProjectController):
         """
         This method creates a MainMenuFrame showing the MainMenu of the application.
 
         Args:
-            _state_manager (state_manager.StateManager): The frame will call the StateManager, if it wants to switch states.
+            state_manager (state_manager.StateManager): The frame will call the StateManager, if it wants to switch states.
             project_controller (project_controller.ProjectController): Respective controller
         """
-        window = super().__init__(state_manager, control)
+        window = super().__init__()
+        self._project_controller = project_controller
+        self._state_manager = state_manager
+
+        self._passive_projects = []
+
+        # Implementing the buttons
 
         customtkinter.CTkButton(self, text="New Project", command=self.__create_project) \
             .pack(side="left", padx=40, pady=40)
@@ -48,4 +61,29 @@ class MainMenuFrame(Activatable, customtkinter.CTkToplevel):
                 .pack(side="right", padx=20, pady=10)  # creates and places the button
 
     def activate(self):
-        pass
+        """
+        Overwrites the activate function from the Activatable-Interface
+        Fetches all the data needed to update the window accordingly
+        """
+        self._passive_projects = self._project_controller.get_list_of_passive_projects()
+
+    def __load_project(self, passive_project: PassiveProject):
+        """
+        Loads the given project
+        Args:
+            passive_project (PassiveProject): Project that will be loaded
+        """
+        project_path = passive_project.get_project_folder_path()
+        self._project_controller.load_project(project_path)
+
+    def __create_project(self):
+        """
+        Calls the create_project-window
+        """
+        self._state_manager.change_state(sne.StateName.CREATE_PROJECT.value)
+
+    def __call_settings(self):
+        """
+        Calls the settings-menu
+        """
+        self._state_manager.change_state(sne.StateName.SETTINGS.value)
--- src/osm_configurator/view/toplevelframes/top_level_frame.py
+++ src/osm_configurator/view/toplevelframes/top_level_frame.py
@@ -5,8 +5,10 @@ import src.osm_configurator.view.states.state_manager
 from abc import ABC, abstractmethod
 from src.osm_configurator.view.activatable import Activatable
 
+from customtkinter import CTkFrame
 
-class TopLevelFrame(Activatable):
+
+class TopLevelFrame(Activatable, CTkFrame):
     """
     This class describes a frame that has a fully developed functionality and that can be placed on a window.
     A TopLevelFrame might have manageable frames below him.
