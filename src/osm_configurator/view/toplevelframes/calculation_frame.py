2724e686ac1a0656cffeb26af088ab1d38e8b47b
uxhoe <weik.felix@gmx.de>
2023-02-01 18:19:46 +0100

    calculation_frame.py can start calculations calculation_states_enum.py was removed due to an already existing enum fulfilling the same purpose

--- src/osm_configurator/view/toplevelframes/calculation_frame.py
+++ src/osm_configurator/view/toplevelframes/calculation_frame.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 import customtkinter
 
+from src.osm_configurator.model.project.calculation.calculation_phase_enum import CalculationPhase
 from src.osm_configurator.view.activatable import Activatable
-from src.osm_configurator.view.states.calculation_states_enum import CalculationStates
 from src.osm_configurator.view.states.state_manager import StateManager
 from src.osm_configurator.control.calculation_controller_interface import ICalculationController
 from src.osm_configurator.control.data_visualization_controller_interface import IDataVisualizationController
@@ -41,9 +41,9 @@ class CalculationFrame(TopLevelFrame, Activatable, customtkinter.CTkToplevel):
              calculation_controller (calculation_controller.CalculationController): Respective controller.
              data_visualization_controller (data_visualization_controller.DataVisualizationController): Respective controller.
         """
-        window = super().__init__()
+        self.window = super().__init__()
 
-        self._starting_point = 0
+        self._starting_point = CalculationPhase.NONE
         self._state_manager = state_manager
         self._calculation_controller = calculation_controller
         self._data_visualization_controller = data_visualization_controller
@@ -51,15 +51,15 @@ class CalculationFrame(TopLevelFrame, Activatable, customtkinter.CTkToplevel):
         #  Creating the entries on the left
 
         self.buttons = [
-            customtkinter.CTkButton(window, text="Data Input and Geofilter", bg_color="grey",
+            customtkinter.CTkButton(master=self.window, text="Data Input and Geofilter", bg_color="grey",
                                     command=self.__data_and_geofilter_pressed),
-            customtkinter.CTkButton(window, text="Tag-Filter", bg_color="grey",
+            customtkinter.CTkButton(master=self.window, text="Tag-Filter", bg_color="grey",
                                     command=self.__tag_filter_pressed),
-            customtkinter.CTkButton(window, text="Reduction", bg_color="grey",
+            customtkinter.CTkButton(master=self.window, text="Reduction", bg_color="grey",
                                     command=self.__reduction_pressed),
-            customtkinter.CTkButton(window, text="Attractivity", bg_color="grey",
+            customtkinter.CTkButton(master=self.window, text="Attractivity", bg_color="grey",
                                     command=self.__attractivity_pressed),
-            customtkinter.CTkButton(window, text="Aggregation", bg_color="grey",
+            customtkinter.CTkButton(master=self.window, text="Aggregation", bg_color="grey",
                                     command=self.__aggregation_pressed)
         ]
 
@@ -68,40 +68,46 @@ class CalculationFrame(TopLevelFrame, Activatable, customtkinter.CTkToplevel):
 
     def activate(self):
         self.__activate_buttons()
+        #  TODO: Keep the progressbar up-to-date
 
     def __tag_filter_pressed(self):
-        self._starting_point = CalculationStates.TAG_FILTER.value
+        self._starting_point = CalculationPhase.TAG_FILTER_PHASE
         self.__disable_buttons()
+        self.__start_calculation()
 
     def __data_and_geofilter_pressed(self):
-        self._starting_point = CalculationStates.DATA_INPUT_AND_GEO_FILTER.value
+        self._starting_point = CalculationPhase.GEO_DATA_PHASE
         self.__disable_buttons()
+        self.__start_calculation()
 
     def __reduction_pressed(self):
-        self._starting_point = CalculationStates.REDUCTION.value
+        self._starting_point = CalculationPhase.REDUCTION_PHASE
         self.__disable_buttons()
+        self.__start_calculation()
 
     def __attractivity_pressed(self):
-        self._starting_point = CalculationStates.ATTRACTIVITY.value
+        self._starting_point = CalculationPhase.ATTRACTIVITY_PHASE
         self.__disable_buttons()
+        self.__start_calculation()
 
     def __aggregation_pressed(self):
-        self._starting_point = CalculationStates.AGGREGATION.value
+        self._starting_point = CalculationPhase.AGGREGATION_PHASE
         self.__disable_buttons()
+        self.__start_calculation()
 
     def __disable_buttons(self):
         """
         Makes a list of buttons non-clickable
         """
         counter = 0
-        starting_point = self._starting_point
+        starting_index = self._starting_point.get_order()
 
         for button in self.buttons:
             button.config(state="disable")
-            if counter == starting_point:
-                button['bg'] = "yellow"
-            elif counter < starting_point:
-                button['bg'] = "green"
+            if counter == starting_index:
+                button['bg_color'] = "yellow"
+            elif counter < starting_index:
+                button['bg_color'] = "green"
             counter += 1
 
     def __activate_buttons(self):
@@ -110,3 +116,25 @@ class CalculationFrame(TopLevelFrame, Activatable, customtkinter.CTkToplevel):
         """
         for button in self.buttons:
             button.config(state="enable")
+
+    def __stop_calculation(self):
+        pass
+
+    def __start_calculation(self):
+        """
+        Starts the calculation:
+            Adds the progressbar and the button to stop the calculation to the window
+            Calls the according controller
+        """
+        self.progressbar = \
+            customtkinter.CTkProgressBar(master=self.window, progress_color="green", width=400,
+                                         orientation="horizontal") \
+            .pack(side="right", padx=40, pady=40)
+
+        cancel_button = \
+            customtkinter.CTkButton(master=self.window, text="Cancel", bg_color="red", command=self.__stop_calculation)\
+            .pack(side="right", padx=40, pady=40)
+
+        self.buttons.append(cancel_button)
+
+        self._calculation_controller.start_calculations(self._starting_point)
