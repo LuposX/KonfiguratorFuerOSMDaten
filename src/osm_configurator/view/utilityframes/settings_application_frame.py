97933bcfbe07e911c6ba139089b462302fa441e8
uxhoe <weik.felix@gmx.de>
2023-02-02 14:17:29 +0100

    Implemented settings_application_frame.py

--- src/osm_configurator/view/utilityframes/settings_application_frame.py
+++ src/osm_configurator/view/utilityframes/settings_application_frame.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 from src.osm_configurator.view.toplevelframes.settings_frame import SettingsFrame
 from src.osm_configurator.control.settings_controller_interface import ISettingsController
 from src.osm_configurator.view.activatable import Activatable
+from src.osm_configurator.view.popups.alert_pop_up import AlertPopUp
 
+from pathlib import Path
 import customtkinter
 
 from typing import TYPE_CHECKING
@@ -12,6 +14,7 @@ if TYPE_CHECKING:
     from src.osm_configurator.view.toplevelframes.settings_frame import SettingsFrame
     from src.osm_configurator.control.settings_controller_interface import ISettingsController
     from src.osm_configurator.view.activatable import Activatable
+    from src.osm_configurator.view.popups.alert_pop_up import AlertPopUp
 
 
 class SettingsApplicationFrame(customtkinter.CTkFrame, Activatable):
@@ -35,12 +38,17 @@ class SettingsApplicationFrame(customtkinter.CTkFrame, Activatable):
         self._project_default_folder = ISettingsController.get_project_default_folder(self._settings_controller)
 
         self.header = customtkinter.CTkLabel(self, text="General Settings")
-        self.header.grid(row=0, column=0, padx=0, pady=0)
+        self.header.grid(row=0, column=0, padx=10, pady=10)
 
-        self.path_default_header = customtkinter.CTkLabel(self, text="Default Folder")
-        self.path_default_box = customtkinter.CTkLabel(self, text=self._project_default_folder.name)
+        self.path_default_header = customtkinter.CTkLabel(self, text="Default Folder") \
+            .grid(row=1, column=0, padx=20, pady=20)
+
+        self.path_default_box = customtkinter.CTkTextbox(self, text=self._project_default_folder.name) \
+            .grid(row=2, column=0, padx=20, pady=20)
+
         self.change_default_path_button = customtkinter.CTkButton(self, text="Change Default Folder",
-                                                                  command=self.__change_default_folder)
+                                                                  command=self.__change_default_folder) \
+            .grid(row=3, column=0, padx=20, pady=20)
 
     def activate(self):
         """
@@ -55,7 +63,17 @@ class SettingsApplicationFrame(customtkinter.CTkFrame, Activatable):
         """
         Changes the default folder to the path that was entered in the textbox
         Checks if the path is valid and confirms changes if so
+        If the path is not valid a popup will be shown reloading the page
         Returns:
-            bool: True if change was successful, else false
+            bool: True if change was successful, else False
         """
-        return True
+        new_path = Path(self.path_default_box.get("0.0", "end"))
+
+        if new_path.exists():
+            self._project_default_folder = new_path
+            ISettingsController.set_project_default_folder(self._settings_controller, new_path)  # updates the path
+            return True
+        popup = AlertPopUp("Please enter a valid path!")
+        popup.mainloop()  # shows the popup
+        self.activate()  # reloads the page
+        return False
