132987328472d5bd7afb68895fe5fcb89171e304
LuposX <schuppsimon5@gmail.com>
2023-02-05 13:00:32 +0100

    implemented recommender_system.py and added a list of tags

--- src/osm_configurator/model/application/recommender_system.py
+++ src/osm_configurator/model/application/recommender_system.py
@@ -1,5 +1,18 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+import pandas as pd
+
+if TYPE_CHECKING:
+    from typing import List, Final
+    from pathlib import Path
+    from pandas import Series
+
+
+# the name of the data entry in the osm tag key file
+CL_KEY: Final = "key"
+
 
 class RecommenderSystem:
 
@@ -13,14 +26,23 @@ class RecommenderSystem:
         """
         pass
 
-    def recommend(self, input):
+    # TODO: remember the path_to the recommender file is saved in the data folder
+    def recommend_key(self, input: str, path_to_recommender_file: Path) -> List[str]:
         """
         Creates recommendations based on user input
 
         Args:
             input (str): The input from which to generate suggestions.
+            path_to_recommender_file (Path):
 
         Returns:
-            list|str]: Returns a list of strings containing the recommendations depending on the input.
+            Series: Returns a Series of strings containing the recommendations depending on the input.
         """
-        pass
+        # open the file
+        key_df = pd.read_csv(path_to_recommender_file)
+
+        # gets a series with true and false
+        # an entry is true if the entry in the dataframe at that position contains the string
+        found_matches: Series = key_df[CL_KEY].str.contains(input)
+
+        return key_df.loc[found_matches]
