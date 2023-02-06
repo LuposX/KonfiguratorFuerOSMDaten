d16ad35170f3dddeebdd2142053f6a2dbeb0f183
LuposX <schuppsimon5@gmail.com>
2023-02-06 16:21:22 +0100

    Added tests for the data visualizer

--- src/osm_configurator/model/project/data_visualizer.py
+++ src/osm_configurator/model/project/data_visualizer.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import os.path
 
+import pandas as pd
+
 import src.osm_configurator.model.model_constants as model_constants_i
 import matplotlib
 
@@ -32,53 +34,47 @@ class DataVisualizer:
         """
         pass
 
-    def create_map(self, cut_out_config: CutOutConfiguration, map_saving_path: Path, filename: str) -> bool:
+    def create_map(self, cut_out_file: Path, map_saving_path: Path, filename: str) -> bool:
         """
         This method is used to create a map from to given cut-out config and save it.
 
         Args:
-            cut_out_config (cut_out_configuration.CutOutConfiguration): The cut-out configuration from which the map should be created.
+            cut_out_file (Path): The cut-out file path.
             map_saving_path (Path): the path where we want to save the file, doesn't include filename.
             filename (str): the name under which the file should be saved, need t ohave the ".html" extension.
 
         Returns:
             bool: True if creating the map works, otherwise false.
         """
-        # Get the cut out file
-        cut_out_file: Path = cut_out_config.get_cut_out_path()
-
-        # read in the cut_out file
-        gdf: GeoDataFrame = gpd.read_file(cut_out_file)
-
-        #get the area to visualize it
-        gdf[model_constants_i.CL_AREA] = gdf.area
-
-        # save the map
-        map: Map = gdf.explode(model_constants_i.CL_AREA, legend=False)
-
         try:
+            gdf: GeoDataFrame = gpd.read_file(cut_out_file)
+            # get the area to visualize it
+            gdf[model_constants_i.CL_AREA] = gdf.area
+
+            # save the map
+            map: Map = gdf.explore(model_constants_i.CL_AREA, legend=False)
             map.save(os.path.join(map_saving_path, filename))
         except Exception:
             return False
 
         return True
 
-    def create_boxplot(self, data: DataFrame, map_saving_path: Path, filename: str) -> bool:
+    def create_boxplot(self, data_path: Path, map_saving_path: Path, filename: str) -> bool:
         """
         This method creates a boxplot which is saved and can later be viewed.
 
         Args:
-            data (DataFrame): The data which we want to visualize, should come from the aggregation phase.
+            data_path (Path): The path towards our data.
             map_saving_path (Path): the path where we want to save the file, doesn't include filename.
             filename (str): the name under which the file should be saved, need to have the ".png" extension.
 
         Returns:
             bool: True if creating the boxplot works, otherwise false.
         """
-        fig = sb.boxplot(data)
-
         # try saving the figure
         try:
+            data: DataFrame = pd.read_csv(data_path)
+            fig = sb.boxplot(data)
             fig.get_figure().savefig(os.path.join(map_saving_path, filename))
         except Exception:
             return False
