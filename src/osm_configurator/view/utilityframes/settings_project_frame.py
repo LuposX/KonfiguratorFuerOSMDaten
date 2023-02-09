ea96787f61da9dcf7bcf17806171fdb07460eb11
uxhoe <weik.felix@gmx.de>
2023-02-09 13:19:36 +0100

    Rearranged placement of widgets in settings_project_frame.py

--- src/osm_configurator/view/utilityframes/settings_project_frame.py
+++ src/osm_configurator/view/utilityframes/settings_project_frame.py
@@ -4,6 +4,7 @@ from src.osm_configurator.view.popups.alert_pop_up import AlertPopUp
 from src.osm_configurator.view.toplevelframes.settings_frame import SettingsFrame
 from src.osm_configurator.control.settings_controller_interface import ISettingsController
 from src.osm_configurator.view.activatable import Activatable
+import src.osm_configurator.view.constants.frame_constants as frame_constants_i
 
 import customtkinter
 
@@ -31,30 +32,42 @@ class SettingsProjectFrame(customtkinter.CTkFrame, Activatable):
             parent (settings_frame.SettingsFrame): The parent of this frame where this frame will be located.
             settings_controller (settings_controller.SettingsController): Respective controller.
         """
-        frame = super().__init__()
+        super().__init__(master=None,
+                         width=frame_constants_i.FrameConstants.FOOT_FRAME_WIDTH.value,
+                         height=frame_constants_i.FrameConstants.FOOT_FRAME_HEIGHT.value,
+                         corner_radius=frame_constants_i.FrameConstants.FRAME_CORNER_RADIUS.value,
+                         fg_color=frame_constants_i.FrameConstants.FOOT_FRAME_FG_COLOR.value)
 
         self._parent = parent
         self._settings_controller = settings_controller
 
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=3)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=3)
+
+        self.grid_rowconfigure(0, weight=3)
+        self.grid_rowconfigure(1, weight=1)
+
         self._project_name = ISettingsController.get_project_name(self._settings_controller)
         self._project_description = ISettingsController.get_project_description(self._settings_controller)
 
-        self.header = customtkinter.CTkLabel(self, text="Current Project") \
-            .pack(side="top")
+        self.header = customtkinter.CTkLabel(master=self, text="Current Project") \
+            .grid(column=0, row=0, rowspan=1, columnspan=1)
 
-        self.project_name_box = customtkinter.CTkTextbox(self, text=self._project_name) \
-            .pack(side="top")
+        self.project_name_box = customtkinter.CTkTextbox(master=self, text=self._project_name) \
+            .grid(column=1, row=0, rowspan=1, columnspan=1)
 
-        self.change_project_name_button = customtkinter.CTkButton(self, text="Change Project Name",
+        self.change_project_name_button = customtkinter.CTkButton(master=self, text="Change Project Name",
                                                                   command=self.__change_project_name) \
-            .pack(side="left")
+            .grid(column=2, row=0, rowspan=1, columnspan=1)
 
         self.description_box = customtkinter.CTkTextbox(self, text=self._project_description) \
-            .pack(side="left")
+            .grid(column=3, row=0, rowspan=1, columnspan=1)
 
         self.change_description_button = customtkinter.CTkButton(self, text="Change Project Description",
                                                                  command=self.__change_project_description) \
-            .pack(side="left")
+            .grid(column=3, row=1, rowspan=1, columnspan=1)
 
     def activate(self) -> bool:
         """
