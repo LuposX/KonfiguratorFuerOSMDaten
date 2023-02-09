162781135c6c66ce76485df8e361a90b9ea50042
Karl Bernhard <karl-bernhard1@gmx.de>
2023-02-09 19:06:26 +0100

    implementing wrong multiprocessing idea

--- src/osm_configurator/model/project/calculation/paralellization/work.py
+++ src/osm_configurator/model/project/calculation/paralellization/work.py
@@ -6,20 +6,38 @@ from typing import Tuple
 
 class Work:
     """
-    A work object consists out of a target function and corresponding arguments
+    A work object consists out of a target function, a function for appoximating the data usage of the task
+    and corresponding arguments.
     """
-    def __init__(self, target: Callable, args: Tuple):
+    def __init__(self, target: Callable, mem_usage: Callable, args: Tuple):
+        """
+        Creates a new work-object (a new task).
+        Args:
+            target (Callable): The actual task. Executing this target-function is the task of the work object
+            mem_usage (Callable): A function which preacceses and approximates the amount of data that needs to be loaded in memory to fulfill the task. Returns the number of bytes needed.
+            args (Tuple): The Argumnts that are passed to the target function, aswell as the mem_usage function
+        """
         self._target: Callable = target
         self._args: Tuple = args
+        self._mem_usage = mem_usage
 
-    def get_target(self) -> Callable:
+    def get_target_function(self) -> Callable:
         """
-        Sets the target of the Work object
+        returns the target function of the Work object
+        """
+        return self._target
+
+    def get_mem_usage_function(self) -> Callable:
+        """
+        Returns the mem_usage function of the Work object. It is a function which preacceses and approximates the
+        amount of data that needs to be loaded in memory to fulfill the task. The function returns the number of
+        bytes needed.
         """
         return self._target
 
     def get_args(self) -> Tuple:
         """
-        Sets the arguments of the work object
+        Gets the arguments of the work object: The Argumnts that are passed to the target function, aswell
+        as the mem_usage function
         """
         return self._args
--- src/osm_configurator/model/project/calculation/paralellization/work_manager.py
+++ src/osm_configurator/model/project/calculation/paralellization/work_manager.py
@@ -5,7 +5,10 @@ import multiprocessing
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from typing import Any
     from typing import List
+    from typing import Tuple
+    from typing import Callable
     from src.osm_configurator.model.project.calculation.paralellization.work import Work
     from multiprocessing import Process
     from multiprocessing import SimpleQueue
@@ -41,11 +44,40 @@ class WorkManager:
         """
         while len(self._work_to_do) > 0:
             if self._can_do_more_work():
-                self._add_worker(self._work_to_do.pop())
-
+                self._do_work(self._work_to_do.pop())
 
     def _can_do_more_work(self) -> bool:
         pass
 
-    def _add_worker(self, work: Work):
-        pass
+    def _do_work(self, work: Work):
+        usage_queue: SimpleQueue = multiprocessing.SimpleQueue()
+        process: Process = multiprocessing.Process(target=self._worker_entry_point,
+                                                   args=(work, self._return_queue, usage_queue,))
+
+        self._workers.append(process)
+        process.start()
+
+        while usage_queue.empty():
+            pass  # Maybe sleep a little
+
+        data_needed: int
+
+
+    def _worker_entry_point(self, work: Work, return_queue: SimpleQueue, usage_queue: SimpleQueue):
+        """
+        This function is the point, where the starting processes start working
+        """
+        # Calculate data needed
+        arguments: Tuple = work.get_args()
+        mem_usage: Callable = work.get_mem_usage_function()
+        data_needed: int = mem_usage(arguments)
+
+        # send the approximated data needed back to the calling process
+        usage_queue.put(data_needed)
+
+        # Execute task and send the return value back to the calling process
+
+        target: Callable = work.get_target_function()
+        return_value: Any = target(arguments)
+
+        return_queue.put(return_value)
