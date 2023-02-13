fe909592417cdb09ec08965b53d04b3e1fd57a99
LuposX <schuppsimon5@gmail.com>
2023-02-13 14:50:31 +0100

    implemented data visualizer controller

--- src/osm_configurator/control/data_visualization_controller.py
+++ src/osm_configurator/control/data_visualization_controller.py
@@ -1,4 +1,7 @@
+import os.path
+
 from src.osm_configurator.control.data_visualization_controller_interface import IDataVisualizationController
+import src.osm_configurator.model.project.calculation.calculation_phase_enum as calculation_phase_enum_i
 
 import pathlib
 import matplotlib
@@ -7,6 +10,11 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from src.osm_configurator.model.application.application_interface import IApplication
+    from pathlib import Path
+    from typing import Final
+
+MAP_FILENAME: Final = "map_of_traffic_cells.html"
+BOXPLOT_FILENAME: Final = "boxplot_of_result.png"
 
 
 class DataVisualizationController(IDataVisualizationController):
@@ -19,10 +27,41 @@ class DataVisualizationController(IDataVisualizationController):
         Args:
             model (application_interface.IApplication): The interface which is used to communicate with the model.
         """
-        pass
+        self._model = model
 
-    def generate_cut_out_map(self) -> pathlib.Path:
-        pass
+    def generate_cut_out_map(self) -> Path | None:
+        cut_out_file: Path = self._model.get_active_project().get_config_manager().get_cut_out_configuration() \
+            .get_cut_out_path()
 
-    def get_calculation_visualization(self) -> matplotlib.axes.Axes:
-        pass
+        map_saving_path: Path = self._model.get_active_project().get_project_path()
+
+        if self._model.get_active_project().get_data_visualizer().create_map(cut_out_file=cut_out_file,
+                                                                             map_saving_path=map_saving_path,
+                                                                             filename=MAP_FILENAME):
+
+            return Path(os.path.join(map_saving_path, MAP_FILENAME))
+
+        # If saving or creating failed
+        else:
+            return None
+
+    def get_calculation_visualization(self) -> Path:
+        boxplot_saving_path: Path = self._model.get_active_project().get_project_path()
+
+        # Get where the data is saved for the results
+        data_path: str = os.path.join(self._model.get_active_project().get_project_path(),
+                                      calculation_phase_enum_i.CalculationPhase.get_folder_name_for_results())
+
+        data_path: Path = Path(os.path.join(data_path,
+                                            calculation_phase_enum_i.CalculationPhase.AGGREGATION_PHASE
+                                            .get_folder_name_for_results()))
+
+        if self._model.get_active_project().get_data_visualizer().create_boxplot(data_path=data_path,
+                                                                                 boxplot_saving_path=boxplot_saving_path,
+                                                                                 filename=BOXPLOT_FILENAME
+                                                                                 ):
+            return Path(os.path.join(data_path, BOXPLOT_FILENAME))
+
+        # If saving or creating failed
+        else:
+            return None
