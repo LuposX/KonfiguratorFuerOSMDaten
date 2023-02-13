bd28b5a890a95256ce34c6b9bc61455949c3e71b
LuposX <schuppsimon5@gmail.com>
2023-02-13 15:49:14 +0100

    application setting will now be saved as json

--- src/osm_configurator/model/application/application_settings.py
+++ src/osm_configurator/model/application/application_settings.py
@@ -5,11 +5,16 @@ from pathlib import Path
 
 from typing import TYPE_CHECKING
 
-import  src.osm_configurator.model.application.application_settings_io_handler as application_settings_io_handler_i
+import src.osm_configurator.model.application.application_settings_io_handler as application_settings_io_handler_i
+import multiprocessing
 
 if TYPE_CHECKING:
     from pathlib import Path
     from src.osm_configurator.model.application.application_settings_io_handler import ApplicationSettingsIOHandler
+    from typing import Final
+
+SETTING_KEY_DEFAULT_PROJECT_FOLDER: Final = "default_project_folder"
+SETTING_KEY_NUMBER_OF_PROCESSES: Final = multiprocessing.cpu_count()
 
 
 class ApplicationSettings:
@@ -25,26 +30,52 @@ class ApplicationSettings:
         Args:
             application_settings_file (Path): name of the file
         """
-        application_settings_io_handler: ApplicationSettingsIOHandler = application_settings_io_handler_i\
+        self._application_settings_io_handler: ApplicationSettingsIOHandler = application_settings_io_handler_i \
             .ApplicationSettingsIOHandler(application_settings_file)
 
-        self.path: Path = application_settings_io_handler.load_settings_file()
+        self._path_settings_file: Path = self._application_settings_io_handler.load_settings_file()
 
-    def get_default_project_folder(self) -> Path:
+    def get_default_project_folder(self) -> Path | None:
         """
         Gives back the path pointing towards the project.
 
         Returns:
             Path: Returns the path of the default location.
+              None: if it failed to read the setting.
         """
-        return self.path
+        try:
+            return Path(self._application_settings_io_handler.get_setting(SETTING_KEY_DEFAULT_PROJECT_FOLDER))
 
-    def set_default_location(self, new_location: Path):
+        except:
+            return None
+
+    def set_default_location(self, new_location: Path) -> bool:
         """
         Sets the default path pointing towards the project to a new Location.
 
         Args:
             new_location (Path): The new Location, where the user wants to save new projects.
+
+        Return:
+            bool: true if sucessfull
         """
-        if os.path.exists(new_location):
-            self.path = new_location
+        try:
+            self._application_settings_io_handler.set_setting(SETTING_KEY_DEFAULT_PROJECT_FOLDER, str(new_location))
+            return True
+
+        except:
+            return False
+
+    def get_number_of_processes(self) -> int | None:
+        """
+        Getter for the number of processes the calculation will be run with.
+
+        Returns:
+            int: Number of processes to use.
+            None: if it failed to read it, this could be because user used an invalid value.
+        """
+        try:
+            return int(self._application_settings_io_handler.get_setting(str(SETTING_KEY_NUMBER_OF_PROCESSES)))
+
+        except:
+            return None
--- src/osm_configurator/model/application/application_settings_io_handler.py
+++ src/osm_configurator/model/application/application_settings_io_handler.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Final
 import pathlib
 
+import json
 
 if TYPE_CHECKING:
     from pathlib import Path
+    from typing import Dict, Any
 
-READ: str = "r"
+READ_MODE: str = "r"
+WRITE_MODE: str = "w"
 
 
 class ApplicationSettingsIOHandler:
@@ -22,17 +25,43 @@ class ApplicationSettingsIOHandler:
         Args:
             application_settings_file (pathlib.Path): name of the file
         """
-        self.filename: Path = application_settings_file
+        self._application_settings_file: Path = application_settings_file
 
-    def load_settings_file(self) -> Path:
+    def get_setting(self, settings_key: str) -> str:
+        """
+        This method gets a specific setting from the setting file.
+
+        Args:
+            settings_key (str): The name of the setting we want to set.
+
+        Returns:
+            str: The value of the setting.
+        """
+        settings: Dict[Any] = self._load_settings_file()
+
+        return settings[settings_key]
+
+    def set_setting(self, settings_key: str, setting_value: str):
+        """
+        This method sets a setting in the setting file.
+
+        Args:
+            settings_key (str): The name of the setting we want to set.
+            setting_value (str): The value we want the setting to have.
+        """
+        settings: Dict[Any] = self._load_settings_file()
+
+        settings[settings_key] = setting_value
+
+        with open(self._application_settings_file, WRITE_MODE) as settings_file:
+            json.dump(settings, settings_file)
+
+    def _load_settings_file(self) -> Dict[Any]:
         """
         This method loads the settings file.
 
         Returns:
-            pathlib.Path: True when building the path works, otherwise false.
+            Dict[Any]: Returns a dictionary of settings.
         """
-        with open(self.filename, READ) as f:
-            location = f.read()
-            return location
-
-
+        with open(self._application_settings_file, READ_MODE) as settings_file:
+            return json.load(settings_file)
