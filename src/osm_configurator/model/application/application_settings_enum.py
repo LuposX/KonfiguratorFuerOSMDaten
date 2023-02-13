0607e7e53488e074e0dd8fff7466ebe279dc121b
LuposX <schuppsimon5@gmail.com>
2023-02-13 16:29:51 +0100

    renamed the enums

--- src/osm_configurator/model/application/application_settings_enum.py
+++ src/osm_configurator/model/application/application_settings_enum.py
@@ -13,8 +13,8 @@ if TYPE_CHECKING:
 class ApplicationSettingsEnum(Enum):
     # The first value of the tuple is teh name of teh setting.
     # The second value is the default value the setting will have if it didn't get changed.
-    SETTING_KEY_DEFAULT_PROJECT_FOLDER = ("default_project_folder", None)
-    SETTING_KEY_NUMBER_OF_PROCESSES = ("number_of_processes_for_calculation", multiprocessing.cpu_count())
+    DEFAULT_PROJECT_FOLDER = ("default_project_folder", None)
+    NUMBER_OF_PROCESSES = ("number_of_processes_for_calculation", multiprocessing.cpu_count())
 
     def get_name(self) -> str:
         """
