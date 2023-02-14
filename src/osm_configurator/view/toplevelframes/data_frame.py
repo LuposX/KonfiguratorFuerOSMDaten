ffda2fcc0d4cd2b4b7a6e649b81c6aa3f8eee1e0
uxhoe <weik.felix@gmx.de>
2023-02-14 12:11:15 +0100

    Implementing base-structure of the buttons

--- src/osm_configurator/view/toplevelframes/data_frame.py
+++ src/osm_configurator/view/toplevelframes/data_frame.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 import customtkinter
 
@@ -9,6 +10,7 @@ import src.osm_configurator.control.cut_out_controller_interface as cut_out_cont
 import src.osm_configurator.control.category_controller_interface as category_controller_interface
 import src.osm_configurator.control.osm_data_controller_interface as osm_data_controller_interface
 from src.osm_configurator.view.activatable import Activatable
+import src.osm_configurator.view.constants.button_constants as button_constants_i
 
 from src.osm_configurator.view.toplevelframes.top_level_frame import TopLevelFrame
 
@@ -19,6 +21,7 @@ if TYPE_CHECKING:
     import src.osm_configurator.control.cut_out_controller_interface as cut_out_controller_interface
     import src.osm_configurator.control.category_controller_interface as category_controller_interface
     import src.osm_configurator.control.osm_data_controller_interface as osm_data_controller_interface
+    import src.osm_configurator.view.constants.button_constants as button_constants_i
 
 
 class DataFrame(TopLevelFrame, Activatable):
@@ -68,5 +71,91 @@ class DataFrame(TopLevelFrame, Activatable):
         self.grid_rowconfigure(4, weight=4)  # Space for the buttons on the right
         self.grid_rowconfigure(5, weight=1)  # Free Space on the right edge
 
+        #  Implementing the buttons
+
+        self._osm_data_select_button: customtkinter.CTkButton = \
+            customtkinter.CTkButton(
+                master=self,
+                corner_radius=button_constants_i.ButtonConstants.BUTTON_CORNER_RADIUS.value,
+                fg_color=button_constants_i.ButtonConstants.BUTTON_FG_COLOR_ACTIVE.value,
+                hover_color=button_constants_i.ButtonConstants.BUTTON_HOVER_COLOR.value,
+                border_color=button_constants_i.ButtonConstants.BUTTON_BORDER_COLOR.value,
+                text_color=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR.value,
+                text_color_disabled=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR_DISABLED.value,
+                command=self.__select_osm_data,
+                text="Select"
+            )
+
+        self._cut_out_select_button: customtkinter.CTkButton = \
+            customtkinter.CTkButton(
+                master=self,
+                corner_radius=button_constants_i.ButtonConstants.BUTTON_CORNER_RADIUS.value,
+                fg_color=button_constants_i.ButtonConstants.BUTTON_FG_COLOR_ACTIVE.value,
+                hover_color=button_constants_i.ButtonConstants.BUTTON_HOVER_COLOR.value,
+                border_color=button_constants_i.ButtonConstants.BUTTON_BORDER_COLOR.value,
+                text_color=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR.value,
+                text_color_disabled=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR_DISABLED.value,
+                command=self.__select_cut_out,
+                text="Select"
+            )
+
+        self._copy_button: customtkinter.CTkButton = \
+            customtkinter.CTkButton(
+                master=self,
+                corner_radius=button_constants_i.ButtonConstants.BUTTON_CORNER_RADIUS.value,
+                fg_color=button_constants_i.ButtonConstants.BUTTON_FG_COLOR_ACTIVE.value,
+                hover_color=button_constants_i.ButtonConstants.BUTTON_HOVER_COLOR.value,
+                border_color=button_constants_i.ButtonConstants.BUTTON_BORDER_COLOR.value,
+                text_color=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR.value,
+                text_color_disabled=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR_DISABLED.value,
+                command=self.__copy_category_configurations,
+                text="Select"
+            )
+
+        self._view_cutout_buttons: customtkinter.CTkButton = \
+            customtkinter.CTkButton(
+                master=self,
+                corner_radius=button_constants_i.ButtonConstants.BUTTON_CORNER_RADIUS.value,
+                fg_color=button_constants_i.ButtonConstants.BUTTON_FG_COLOR_ACTIVE.value,
+                hover_color=button_constants_i.ButtonConstants.BUTTON_HOVER_COLOR.value,
+                border_color=button_constants_i.ButtonConstants.BUTTON_BORDER_COLOR.value,
+                text_color=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR.value,
+                text_color_disabled=button_constants_i.ButtonConstants.BUTTON_TEXT_COLOR_DISABLED.value,
+                command=self.__view_cut_out,
+                text="Select"
+            )
+
     def activate(self):
+        pass
+
+    def __view_cut_out(self):
+        """
+        Lets the user view the cutout.
+        Activated if the view_cutout button is activated
+        """
+        pass
+
+    def __copy_category_configurations(self):
+        pass
+
+    def __select_cut_out(self):
+        """
+        Opens the explorer letting the user choose a file selecting the cut-out
+        """
+        pass
+
+    def __select_osm_data(self):
+        """
+        Opens the explorer letting the user choose a file selecting the osm-data
+        """
+        pass
+
+    def __chose_file(self, filetype: str) -> Path:
+        """
+        Opens the explorer letting the user choose a file
+        Args:
+            filetype (str): Accepted types of the files
+        Returns:
+            Path: Path of the chosen file
+        """
         pass
