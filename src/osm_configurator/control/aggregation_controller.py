e1a686380271104138a8b28822051ee8476f1473
jphan <ujrlh@student.kit.edu>
2023-02-15 18:59:47 +0100

    aggregation controller

--- src/osm_configurator/control/aggregation_controller.py
+++ src/osm_configurator/control/aggregation_controller.py
@@ -17,13 +17,13 @@ class AggregationController(IAggregationController):
         Args:
             model (application_interface.IApplication): The interface which is used to communicate with the model.
         """
-        pass
+        self._aggregation_configurator = model.get_active_project().get_config_manager().get_aggregation_configuration()
 
     def get_aggregation_methods(self) -> list[AggregationMethod]:
-        pass
+        return self._aggregation_configurator.get_all_aggregation_methods()
 
     def is_aggregation_method_active(self, method: AggregationMethod) -> bool:
-        pass
+        return self._aggregation_configurator.is_aggregation_method_active(method)
 
     def set_aggregation_method_active(self, method: AggregationMethod, active: bool) -> bool:
-        pass
+        return self._aggregation_configurator.set_aggregation_method_active(method, active)
