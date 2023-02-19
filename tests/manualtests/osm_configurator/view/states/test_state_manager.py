692c5b446a0571595b3c062daa7f676a71bd89fe
uxhoe <weik.felix@gmx.de>
2023-02-19 14:47:04 +0100

    Started testing

--- tests/manualtests/osm_configurator/view/states/test_state_manager.py
+++ tests/manualtests/osm_configurator/view/states/test_state_manager.py
@@ -2,6 +2,23 @@ import pytest
 
 from src.osm_configurator.view.states.state_manager import StateManager
 
+from tests.manualtests.osm_configurator.view.controller_stub.export_controller_stub import ExportControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.category_controller_stub import CategoryControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.project_controller_stub import ProjectControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.settings_controller_stub import SettingsControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.aggregation_controller_stub import \
+    AggregationControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.cut_out_controller_stub import CutOutControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.data_visualization_controller_stub import \
+    DataVisualizationControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.osm_data_controller_stub import OSMDataControllerStub
+from tests.manualtests.osm_configurator.view.controller_stub.calculation_controller_stub import \
+    CalculationControllerStub
+
+from src.osm_configurator.view.states.main_window import MainWindow
+from src.osm_configurator.view.states.state_name_enum import StateName
+
+
 def test_innit():
     pass
 
