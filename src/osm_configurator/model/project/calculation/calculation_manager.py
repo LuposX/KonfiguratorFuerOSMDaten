476da9588ccdc2b925d1b7982b900330493c3f70
Karl Bernhard <karl-bernhard1@gmx.de>
2023-02-22 14:38:22 +0100

    removed debug prints

--- src/osm_configurator/model/project/calculation/calculation_manager.py
+++ src/osm_configurator/model/project/calculation/calculation_manager.py
@@ -150,8 +150,6 @@ class CalculationManager:
         while current_index < len(self._phases) and result[0] == calculation_state_enum.CalculationState.RUNNING:
             phase_queue.put(self._phases[current_index].get_calculation_phase_enum())
             result = self._phases[current_index].calculate(self._config_manager, self._application_manager)
-            print(result, flush=True)
-            print(self._phases[current_index].get_calculation_phase_enum().get_name(), flush=True)
             state_queue.put(result)  # Put the return value of the phases in the queue to the main process
             current_index += 1
 
