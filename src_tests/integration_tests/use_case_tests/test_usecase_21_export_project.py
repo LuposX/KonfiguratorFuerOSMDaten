e212138d050bcf9b5277b7e3a714faad01f03293
jphan <ujrlh@student.kit.edu>
2023-03-08 11:55:01 +0100

    fix asserts in tests

--- src_tests/integration_tests/use_case_tests/test_usecase_21_export_project.py
+++ src_tests/integration_tests/use_case_tests/test_usecase_21_export_project.py
@@ -6,10 +6,12 @@ from src_tests.definitions import TEST_DIR
 import src.osm_configurator.model.application.application_settings as application_settings_i
 
 
-class TestUseCase22:
-    def test_export_config(self):
+class TestUseCase21:
+
+    def test_export_project(self):
         application_settings_o = application_settings_i.ApplicationSettings()
         self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True,
-                                                           application_settings_o, "Project_UC22", "Das sollte funktionieren")
-        assert self.active_project.get_export_manager().export_configuration(
-            Path(os.path.join(TEST_DIR, "build/Export/UC22")))
+                                                           application_settings_o, "Project_UC22",
+                                                           "Das sollte funktionieren")
+        self.active_project.get_export_manager().export_project(
+            Path(os.path.join(TEST_DIR, "build/Export/UC21")))
--- src_tests/unittests/osm_configurator/model/project/test_export.py
+++ src_tests/unittests/osm_configurator/model/project/test_export.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
 import os
-import shutil
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 from src.osm_configurator.model.project.active_project import ActiveProject
-from src.osm_configurator.model.project.calculation import file_deletion
 from src_tests.definitions import TEST_DIR
 import src.osm_configurator.model.application.application_settings as application_settings_i
 
@@ -16,19 +13,21 @@ class TestExport:
     def test_export_project(self):
         application_settings_o = application_settings_i.ApplicationSettings()
         self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
-        self.active_project.get_export_manager().export_project(Path(os.path.join(TEST_DIR, "build/Export/Project")))
+        assert self.active_project.get_export_manager().export_project(Path(os.path.join(TEST_DIR, "build/Export/Project")))
 
     def test_export_configuration(self):
         application_settings_o = application_settings_i.ApplicationSettings()
         self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
-        self.active_project.get_export_manager().export_configuration(Path(os.path.join(TEST_DIR, "build/Export/Configuration")))
+        assert self.active_project.get_export_manager().export_configuration(Path(os.path.join(TEST_DIR, "build/Export/Configuration")))
 
     def test_export_calculation(self):
         application_settings_o = application_settings_i.ApplicationSettings()
         self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
-        self.active_project.get_export_manager().export_calculation(Path(os.path.join(TEST_DIR, "build/Export/Calculation")))
+        assert self.active_project.get_export_manager().export_calculation(Path(os.path.join(TEST_DIR, "build/Export/Calculation")))
 
     def test_export_map(self):
         application_settings_o = application_settings_i.ApplicationSettings()
         self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
-        self.active_project.get_export_manager().export_map(Path(os.path.join(TEST_DIR, "build/Export/Map")))
+        self.active_project.get_config_manager().get_osm_data_configuration().set_osm_data(Path(os.path.join(TEST_DIR, "data/monaco-latest.osm")))
+        self.active_project.get_config_manager().get_cut_out_configuration().set_cut_out_path(Path(os.path.join(TEST_DIR, "data/monaco-regions.geojson")))
+        assert self.active_project.get_export_manager().export_map(Path(os.path.join(TEST_DIR, "build/Export/Map")))
