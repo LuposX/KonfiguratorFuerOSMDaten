8961dbb24f545874eeda9222dd732f547eebf10c
jphan <ujrlh@student.kit.edu>
2023-03-08 11:00:20 +0100

    fix export tests

--- src_tests/unittests/osm_configurator/model/project/test_export.py
+++ src_tests/unittests/osm_configurator/model/project/test_export.py
@@ -8,39 +8,27 @@ from typing import TYPE_CHECKING
 from src.osm_configurator.model.project.active_project import ActiveProject
 from src.osm_configurator.model.project.calculation import file_deletion
 from src_tests.definitions import TEST_DIR
-
-if TYPE_CHECKING:
-    from src.osm_configurator.model.project.calculation.file_deletion import FileDeletion
-
-
-def _prepare_project_folder(path_to_new_project: Path, path_old_data: Path):
-    # Prepare result folder
-    deleter: FileDeletion = file_deletion.FileDeletion()
-    deleter.reset_folder(path_to_new_project)
-
-    # move the files from data to it
-    try:
-        shutil.copytree(path_old_data, path_to_new_project)
-    except:
-        pass
-    with open(os.path.join(path_to_new_project, "application_settings.json"), "w") as file:
-        file.write(str(path_to_new_project))
+import src.osm_configurator.model.application.application_settings as application_settings_i
 
 
 class TestExport:
 
     def test_export_project(self):
-        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, "TestProjectExport", "Das sollte funktionieren")
+        application_settings_o = application_settings_i.ApplicationSettings()
+        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
         self.active_project.get_export_manager().export_project(Path(os.path.join(TEST_DIR, "build/Export/Project")))
 
     def test_export_configuration(self):
-        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, "TestProjectExport", "Das sollte funktionieren")
+        application_settings_o = application_settings_i.ApplicationSettings()
+        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
         self.active_project.get_export_manager().export_configuration(Path(os.path.join(TEST_DIR, "build/Export/Configuration")))
 
     def test_export_calculation(self):
-        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, "TestProjectExport", "Das sollte funktionieren")
+        application_settings_o = application_settings_i.ApplicationSettings()
+        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
         self.active_project.get_export_manager().export_calculation(Path(os.path.join(TEST_DIR, "build/Export/Calculation")))
 
     def test_export_map(self):
-        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, "TestProjectExport", "Das sollte funktionieren")
+        application_settings_o = application_settings_i.ApplicationSettings()
+        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True, application_settings_o, "TestProjectExport", "Das sollte funktionieren")
         self.active_project.get_export_manager().export_map(Path(os.path.join(TEST_DIR, "build/Export/Map")))
