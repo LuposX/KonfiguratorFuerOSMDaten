c7e000fc9b19487f5b13525c8efd08e2fedab262
jphan <ujrlh@student.kit.edu>
2023-03-09 10:14:18 +0100

    begin test 12

---
+++ src_tests/integration_tests/use_case_tests/test_usecase_12_define_reduction.py
@@ -0,0 +1,6 @@
+from src.osm_configurator.model.project.active_project import ActiveProject
+
+
+class TestUsecase12:
+    def test_usecase_descriptiion_1(self):
+        active_project: ActiveProject = ActiveProject("")
