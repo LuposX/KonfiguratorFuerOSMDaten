bc27f72e308c0da4bd9b4ac00a9217fafd877b6d
jphan <ujrlh@student.kit.edu>
2023-03-09 10:40:40 +0100

    test 13 done

---
+++ src_tests/integration_tests/use_case_tests/test_use_case_13_define_default_value_entry.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+from src.osm_configurator.model.project.active_project import ActiveProject
+import src.osm_configurator.model.application.application_settings as application_settings_i
+from src.osm_configurator.model.project.configuration.attribute_enum import Attribute
+from src.osm_configurator.model.project.configuration.category import Category
+from src.osm_configurator.model.project.configuration.default_value_entry import DefaultValueEntry
+from src_tests.definitions import TEST_DIR
+
+
+class TestUsecase13:
+    def test_usecase(self):
+        application_settings_o = application_settings_i.ApplicationSettings()
+        self.active_project: ActiveProject = ActiveProject(Path(os.path.join(TEST_DIR, "build/Projects")), True,
+                                                           application_settings_o, "TestDefaultValues")
+        test_category: Category = Category("CategoryOne")
+        self.active_project.get_config_manager().get_category_manager().create_category(test_category)
+
+        # Description part 3
+        new_default_value_entry_one: DefaultValueEntry = DefaultValueEntry("TestTag1")
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").add_default_value_entry(new_default_value_entry_one)
+        new_default_value_entry_two: DefaultValueEntry = DefaultValueEntry("TestTag2")
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").add_default_value_entry(new_default_value_entry_two)
+
+        # Desciption part 4
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].get_default_value_entry_tag() == "TestTag1"
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].get_attribute_default(Attribute.FLOOR_AREA) == 0.0
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].get_attribute_default(Attribute.NUMBER_OF_FLOOR) == 0.0
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].get_attribute_default(Attribute.PROPERTY_AREA) == 0.0
+
+        # Desciption part 5
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].set_tag("NewNameForTag1")
+
+        # Desciption part 6
+        assert not self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").move_default_value_entry_up(new_default_value_entry_one)
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").move_default_value_entry_up(new_default_value_entry_two)
+
+        # Desciption part 7
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].set_attribute_default(Attribute.FLOOR_AREA, 5.0)
+        assert self.active_project.get_config_manager().get_category_manager().get_category(
+            "CategoryOne").get_default_value_list()[1].get_attribute_default(Attribute.FLOOR_AREA) == 5.0
+
