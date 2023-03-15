69a04a5805ed0a2abed7c01cb6cab5b1a74cc1f7
LuposX <36456825+LuposX@users.noreply.github.com>
2023-03-15 16:54:08 +0100

    Testcase18 #334

---
+++ src_tests/integration_tests/use_case_tests/use_case_18_visualize_calculation.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+import pytest
+from typing import TYPE_CHECKING
+
+from pathlib import Path
+import os
+from src_tests.definitions import TEST_DIR
+
+import src.osm_configurator.control.category_controller as category_controller
+import src.osm_configurator.control.aggregation_controller as aggregation_controller
+import src.osm_configurator.control.project_controller as project_controller
+import src.osm_configurator.control.data_visualization_controller as data_visualization_controller
+import src.osm_configurator.model.application.application as application
+import src.osm_configurator.model.project.calculation.file_deletion as file_deletion
+
+if TYPE_CHECKING:
+    from src.osm_configurator.control.category_controller import ICategoryController
+    from src.osm_configurator.control.project_controller_interface import IProjectController
+    from src.osm_configurator.control.aggregation_controller_interface import IAggregationController
+    from src.osm_configurator.model.application.application_interface import IApplication
+
+
+class TestUseCase18:
+    def test_successful_category_import(self):
+        # Create Model and Controller
+        model: IApplication = application.Application()
+        project_ctrl: IProjectController = project_controller.ProjectController(model)
+        category_ctrl: ICategoryController = category_controller.CategoryController(model)
+        aggregation_ctrl: IAggregationController = aggregation_controller.AggregationController(model)
+        data_viz_controller = data_visualization_controller.DataVisualizationController(model)
+
+        # Load project
+        assert not project_ctrl.is_project_loaded()
+        assert project_ctrl.load_project(
+            Path(os.path.join(TEST_DIR, "data/UseCase18")))
+        assert project_ctrl.is_project_loaded()
+
+        # Test if visualization is successful
+        try:
+            data_viz_controller.generate_calculation_visualization()
+        except Exception as ex:
+            assert False, "raised an exception" + str(ex.args)
+
+        try:
+            data_viz_controller.generate_cut_out_map()
+        except Exception as ex:
+            assert False, "raised an exception" + str(ex.args)
